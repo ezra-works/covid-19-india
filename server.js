a531676ef2566a2becd852d48e4373af651d9ca4
Ezra Moses <emosesrajkumar@woolworths.com.au>
2020-05-10 21:31:41 +1000

    adding geojson files

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const scrapdata = require('./routes/ScrapeData');
 const app = express();
+const path = require('path');
 
 app.use(express.json());
 
 // Use routes
 app.use('/data', scrapdata);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 const port = process.env.PORT || 5000;
 
